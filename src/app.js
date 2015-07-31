6c980bd25f340a5c831750f3d28094eb6ca6c9f8
Mandy Pao <meeiyu@hotmail.com>
2015-07-31 12:02:42 +0800

    Add highcharts.

--- src/app.js
+++ src/app.js
@@ -1,45 +1,123 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/app.scss';
-
+// lib
 import React from 'react';
-import Modal from './components/Modal/Modal';
 
 class App extends React.Component {
+    componentDidMount() {
+        $(React.findDOMNode(this.refs.container)).highcharts({
+            chart: {
+                type: 'line',
+                zoomType: 'xy',
+                spacingBottom: 50
+            },
+            title: {
+                text: '服務經營詳細數據'
+            },
+            xAxis: [{
+                categories: ['7月9日', '7月10日', '7月11日', '7月12日', '7月13日', '7月14日', '7月15日', '7月16日', '7月17日', '7月18日', '7月19日', '7月20日', '7月21日', '7月22日', '7月23日', '7月24日', '7月25日', '7月26日', '7月27日', '7月28日', '7月29日', '7月30日'],
+                crosshair: true
+            }],
+            yAxis: [
+            {
+                // Primary yAxis (UV)
+                title: {
+                    text: 'UV',
+                    enabled: true,
+                    style: {
+                        color: Highcharts.getOptions().colors[2]
+                    }
+                },
+                min: 0,
+                max: 250,
+                labels: {
+                    enabled: true,
+                    style: {
+                        color: Highcharts.getOptions().colors[2]
+                    }
+                },
+                opposite: true
+            },
+            {   // Secondary yAxis
+                // gridLineWidth: 0,
+                min: 0,
+                max: 30,
+                title: {
+                    text: '訂購數量',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                labels: {
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                }
+            },
+            {   // Tertiary yAxis (PV)
+                // gridLineWidth: 0,
+                title: {
+                    text: 'PV',
+                    enabled: true,
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                min: 0,
+                max: 250,
+                labels: {
+                    enabled: true,
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                opposite: true
+            }],
+            tooltip: {
+                shared: true
+            },
+            legend: {
+                borderColor: '#aaa',
+                borderWidth: 1,
+                borderRadius:3,
+                layout: 'horizontal',
+                align: 'center',
+                verticalAlign: 'bottom',
+                x: 80,
+                y: 40,
+                floating: true,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+            },
+            series: [{
+                name: '訂購筆數',
+                type: 'column',
+                //color: '#00FF00',
+                yAxis: 1,
+                data: [9, 7, 12, 3, 8, 25, 12, 5, 1, 0, 0, 1, 1, 12, 1, 1, 3, 1, 0, 2, 1, 5],
+                tooltip: {
+                    valueSuffix: '筆'
+                }
+
+            }, {
+                name: 'PV',
+                type: 'spline',
+                //color: '#00FF00',
+                yAxis: 2,
+                data: [46, 30, 58, 5, 72, 160, 150, 120, 125, 51, 245, 132, 60, 196, 87, 46, 21, 7, 19, 31, 16, 44]
+
+            }, {
+                name: 'UV',
+                type: 'spline',
+                //color: '#00FF00',
+                data: [29, 23, 36, 4, 39, 101, 91, 64, 76, 38, 213, 105, 49, 119, 66, 32, 15, 6, 14, 22, 16, 35]
+            }]
+        });
+    }
     render() {
         return (
-        	<div className="container">
-                <div className="jumbotron">
-                    <div className="app-jumbotron-header-wrapper">
-                        <h1 className="app-jumbotron-header">Hello Webpack!!</h1>
-                        <p className="app-jumbotron-body">Webpack boiler plate using proxies with webpack-dev-server</p>
-                    </div>
-                    <div className="app-jumbotron-image"></div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-4">
-                        <h3>Column 1</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-                        <span className="glyphicon glyphicon-tag"></span>
-                        <span className="glyphicon glyphicon-screenshot"></span>
-                        <span className="glyphicon glyphicon-thumbs-up"></span>
-                    </div>
-                    <div className="col-sm-4">
-                        <h3>Column 2</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-                        <Modal />
-                    </div>
-                    <div className="col-sm-4">
-                        <h3>Column 3</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-                    </div>
-                </div>
-            </div>
+            <div className="container" ref="container" />
         );
     }
 }
 
-console.log(App);
 React.render(<App />, document.body);
